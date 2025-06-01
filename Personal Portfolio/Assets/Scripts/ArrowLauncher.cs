d7076d00e9f0c6f0549eb100a6efe1a10a6aea98
Aleksandar Georgiev <549190@student.saxion.nl>
2025-06-01 17:08:52 +0200

    Started working on Arrow

---
+++ Personal Portfolio/Assets/Scripts/ArrowLauncher.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class ArrowLauncher : MonoBehaviour
+{
+    [Header("Launch Settings)")]
+    [SerializeField] private float speed = 10f;
+
+    private Rigidbody rb;
+    private bool inAir = false;
+    private XRPullInteractable _pullInteractable;
+
+    private void Awake()
+    {
+        InitializeComponents();
+        SetPhysics(false);
+    }
+
+    private void InitializeComponents()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if(rb == null)
+        {
+            Debug.LogError($"RigidBody not found in {gameObject.name}");
+        }
+    }
+
+    public void Initialize(XRPullInteractable pullInteractable)
+    {
+        _pullInteractable = pullInteractable;
+        _pullInteractable.OnPullReleased += Release;
+    }
+
+    private void OnDestroy()
+    {
+        if( _pullInteractable != null )
+        {
+            _pullInteractable.OnPullReleased -= Release;
+        }
+    }
+
+    private void Release(float value)
+    {
+        if(_pullInteractable != null)
+        {
+            _pullInteractable.OnPullReleased -= Release;
+        }
+
+        gameObject.transform.parent = null;
+        inAir = false;
+        SetPhysics(true);
+
+        Vector3 force = transform.forward * value * speed;
+        rb.AddForce(force, ForceMode.Impulse);
+
+        StartCoroutine(RotateWithVelocity());
+    }
+
+    private IEnumerator RotateWithVelocity()
+    {
+        yield return new WaitForFixedUpdate();
+
+        while(inAir)
+        {
+            if(rb != null && rb.linearVelocity.sqrMagnitude > 0.01f)
+            {
+                transform.rotation = Quaternion.LookRotation(rb.linearVelocity, transform.up);
+            }
+            yield return null;
+        }
+    }
+
+    private void StopFlight()
+    {
+        inAir = false;
+        SetPhysics(false);
+    }
+
+    private void SetPhysics(bool state)
+    {
+        if(rb != null)
+        {
+            rb.useGravity = state;
+            rb.isKinematic = !state;
+        }
+    }
+}
