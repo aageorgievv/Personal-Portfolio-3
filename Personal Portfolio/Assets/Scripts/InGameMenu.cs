aef98ee8e354aed3e3f46047a25fb21bd7053836
Aleksandar Georgiev <549190@student.saxion.nl>
2025-07-05 22:07:52 +0200

    InGame Menu Done (Made Custom Input Action)

--- Personal Portfolio/Assets/Scripts/InGameMenu.cs
+++ Personal Portfolio/Assets/Scripts/InGameMenu.cs
@@ -1,22 +1,56 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InGameMenu : MonoBehaviour
 {
     [SerializeField] private GameObject menu;
-    private InputActionProperty toggleButton;
-    void Start()
-    {
+    public InputActionReference openMenuAction;
 
+    private void OnEnable()
+    {
+        openMenuAction.action.Enable();
+        openMenuAction.action.performed += OnToggleMenu;
+        InputSystem.onDeviceChange += OnDeviceChange;
     }
 
-    void Update()
+    private void OnDisable()
     {
-
+        openMenuAction.action.Disable();
+        openMenuAction.action.performed -= OnToggleMenu;
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+    private void OnToggleMenu(InputAction.CallbackContext context)
+    {
+        if (menu != null)
+        {
+            menu.SetActive(!menu.activeSelf);
+        }
     }
 
-    private void ToggleMenu(InputAction.CallbackContext context)
+    private void OnDeviceChange(InputDevice input, InputDeviceChange change)
     {
-        menu.SetActive(!menu.activeSelf);
+        switch (change)
+        {
+            case InputDeviceChange.Disconnected:
+                openMenuAction.action.Disable();
+                openMenuAction.action.performed -= OnToggleMenu;
+                break;
+            case InputDeviceChange.Reconnected:
+                openMenuAction.action.Enable();
+                openMenuAction.action.performed += OnToggleMenu;
+                break;
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
